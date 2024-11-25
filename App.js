e4e76e87079df7577e0150123cd6b94e6a08a243
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-11-25 14:46:35 +0200

    pelkkä footer näkösälle

--- App.js
+++ App.js
@@ -7,12 +7,12 @@ import useTheme from './hooks/Theme'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Footer from './components/Footer'
-import Homescreen from './screens/HomeScreen'
-import WeatherScreen from './screens/WeatherScreen'
-import ListScreen from './screens/ListScreen'
-import ItemScreen from './screens/ChosenListScreen'
-import CurrencyCalculator from './screens/CalculatorScreen'
-import CheckCredentials from './screens/CheckCredentials'
+//import Homescreen from './screens/HomeScreen'
+//import WeatherScreen from './screens/WeatherScreen'
+//import ListScreen from './screens/ListScreen'
+//import ItemScreen from './screens/ChosenListScreen'
+//import CurrencyCalculator from './screens/CalculatorScreen'
+//import CheckCredentials from './screens/CheckCredentials'
 import { navigatorStyles, appStyles as styles } from './styles/Styles'
 
 
@@ -32,19 +32,6 @@ export default function App() {
     <NavigationContainer>
       <PaperProvider theme={theme}>
         <SafeAreaView style={styles.safeArea}>
-          {logged ? (
-          <Stack.Navigator initialRouteName='Lists' screenOptions={navigatorStyles(theme)}>
-            <Stack.Screen style={styles.screen} name="Lists" component={ListScreen} />
-            <Stack.Screen style={styles.screen} name="Items" component={ItemScreen} />
-            <Stack.Screen style={styles.screen} name="Home">
-              {props => <Homescreen {...props} toggleTheme={toggleTheme} />}
-            </Stack.Screen>
-            <Stack.Screen style={styles.screen} name="Weather" component={WeatherScreen} />
-            <Stack.Screen style={styles.screen} name="Currency" component={CurrencyCalculator} />           
-          </Stack.Navigator>
-          ) : (
-            <CheckCredentials setLogged={setLogged} />
-          )}
           <Footer style={styles.footer} />
         </SafeAreaView>
       </PaperProvider>
